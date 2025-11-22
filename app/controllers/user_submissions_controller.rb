7db36d25a87f19421e40bd4692bef14818ee299c
alexose <alexose@users.noreply.github.com>
2025-11-22 13:27:36 -0800

    Add pagination for Nearby Activity

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -4,8 +4,9 @@ class UserSubmissionsController < ApplicationController
   def list_within_range
     user_submissions = UserSubmission.where.not(lat: nil)
                                      .where(submission_type: %w[new_lmx remove_machine new_condition confirm_location], created_at: "2019-05-03T07:00:00.00-07:00"..Date.today.end_of_day)
-                                     .near([ params[:lat], params[:lon] ], 100, order: false).limit(200)
-    sorted_submissions = user_submissions.order("created_at DESC")
-    render partial: "maps/nearby_activity", locals: { sorted_submissions: sorted_submissions }
+                                     .near([ params[:lat], params[:lon] ], 100, order: false)
+                                     .order("created_at DESC")
+    @pagy, sorted_submissions = pagy(user_submissions, items: 10)
+    render partial: "maps/nearby_activity", locals: { sorted_submissions: sorted_submissions, pagy: @pagy }
   end
 end
